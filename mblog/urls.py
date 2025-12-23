26b106548a1280fd71ce7157d663277157762b74
jeffcth13 <jeffcth13@gmail.com>
2025-12-23 23:33:25 +0800

    finish at 2-24

--- mblog/urls.py
+++ mblog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import homepage
+from mysite.views import homepage, showpost
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', homepage),
+    path('post/<slug:post_slug>/', showpost),
+    path('admin/', admin.site.urls),
 ]
